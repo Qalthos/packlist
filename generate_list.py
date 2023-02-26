0d9287880ae5e5119e0f997626358d6a8b0762c9
Kate Case <this.is@katherineca.se>
2023-02-26 14:29:41 -0500

    Initial commit with JoCo 2023 list

---
+++ generate_list.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+import yaml
+
+Subpocket = dict[str, str]
+Pocket = list[str | Subpocket]
+Container = dict[str, Pocket]
+
+
+def print_pocket(pocket: Pocket | Subpocket, /, prefix: str = ""):
+    for item in pocket:
+        if isinstance(item, dict):
+            for name, subpocket in item.items():
+                print(f"{prefix}* {name}:\n")
+                print_pocket(subpocket, prefix=prefix + "  ")
+        else:
+            print(f"{prefix}* ☐ {item}")
+
+
+def print_container(container: Container):
+    for name, pocket in container.items():
+        if pocket:
+            print(f"{name}\n{'-' * len(name)}\n")
+            print_pocket(pocket)
+            print()
+
+
+def main():
+    config = Path("items.yaml")
+    with open(config, "r") as yaml_file:
+        items: dict[str, Container | Pocket] = yaml.safe_load(yaml_file)
+
+    for name, container in items.items():
+        print(f"{name}\n{'=' * len(name)}\n")
+        if isinstance(container, dict):
+            print_container(container)
+        else:
+            print_pocket(container)
+            print()
+
+
+if __name__ == "__main__":
+    main()
